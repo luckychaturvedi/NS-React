44ab1eed5cd6f606090dda2d6aa2e05db2ddff86
lucky <luckychaturvedi007@gmail.com>
2024-10-16 14:12:49 +0530

    package json

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-// 
-// 
-// 
-
-
-
+import React from "react";
+import ReactDOM from "react-dom/client"
 
 
 const heading = React.createElement("h6", 
     {id: "heading", abc: "xyz"}, "value of div",  
     [ React.createElement("p", {className : "child"}, "value of child"),
-        React.createElement("p", {className : "child"}, "value of child"),
-        React.createElement("p", {className : "child2"}, "value of child"),
-        React.createElement("p", {className : "child3"}, "value of child"),
+        React.createElement("p", {className : "child"}, "this of child"),
+       
         React.createElement("p", {className : "child4"}, "value of child")
     ],
-        React.createElement("a", {href: "www.google.com", target: "_blank"}, "test link" )
+        React.createElement("a", {href: "https://www.google.com/", target: "_blank"}, "test link" )
 
 );
 
